a641e19820db16bd9e17f11516b8e9b95de5204f
minjung609 <emilywin825@gmail.com>
2022-03-29 20:15:17 +0900

    first commit

---
+++ Chapter02/src/ComperisonOperator.java
@@ -0,0 +1,11 @@
+
+public class ComperisonOperator {
+public static void main(String[] args) {
+		
+		int a=3;
+		int b=6;
+		System.out.println((a==b));
+		System.out.println((a==1)&&(b==6));
+		}
+
+}
