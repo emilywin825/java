464f3f93641726343a01df8d25304a5cd7ab8f12
minjung609 <emilywin825@gmail.com>
2022-03-29 20:17:39 +0900

    commit

--- Chapter02/src/TypeConversion.java
+++ Chapter02/src/TypeConversion.java
@@ -1,4 +1,20 @@
 
 public class TypeConversion {
+    //강제적 형변환
+		public static void main(String[] args)
+		{
+		  double a,b;
+		  int c;
+		  a=1.4;
+		  b=1.6;
+		  c=6;
+		  
+		  int sum;
+		  sum=(int)a+(int)b;
+		  System.out.println(sum);
+		  
+		  int sum2;
+		  sum2=(int)a+c;
+		  System.out.println(sum2);}
 
 }
